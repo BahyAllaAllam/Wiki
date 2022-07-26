cfe8800e335dc6e89524b77d83269731bf6804b1
BahyAlaaAllam <bahy.alam@yahoo.com>
2022-07-26 19:18:51 +0200

    Add the create and edit functionality

--- src/encyclopedia/urls.py
+++ src/encyclopedia/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'encyclopedia'
+
 urlpatterns = [
     path("", views.home, name="home"),
     path("wiki/<str:title>", views.detail, name="detail"),
     path("search/", views.search, name="search"),
-    path("random-page/", views.rand, name="random")
+    path("random-page/", views.rand, name="random"),
+    path("create/", views.create, name="create"),
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
+
 ]
--- src/encyclopedia/views.py
+++ src/encyclopedia/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import Http404
 from . import util
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 import random
+from django.contrib import messages
+
 def home(request):
-    return render(request, "encyclopedia/home.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/home.html", { "entries": util.list_entries() })
 
 def detail(request, title):
     entry = util.get_entry(title)
+    context= {"entry": entry, 'title':title}
     if entry:
-        return render(request, "encyclopedia/detail.html", {
-            "entry": entry
-        })
+        return render(request, "encyclopedia/detail.html", context)
     else:
         return HttpResponseNotFound("<h1>requested page was not found</h1>")
 
@@ -46,3 +45,37 @@ def rand(request):
     entry = random.choice(entries)
 
     return detail(request, entry)
+
+def create(request):
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        entries = util.list_entries()
+        context = {}
+        for entry in entries:
+            if title.lower() == entry.lower():
+                messages.warning(request, "This entry already exist")
+                context = {'title':title, 'content':content}
+                return render(request, "encyclopedia/create.html", context)
+                # to redirect to the same page
+                #return HttpResponseRedirect(request.path_info)
+
+        util.save_entry(title, content)
+        messages.success(request, 'Your entry has been add successfully !')
+        return redirect('encyclopedia:detail', title)
+
+    return render(request, "encyclopedia/create.html")
+
+
+def edit(request, title):
+    content = util.get_entry(title)
+
+    if request.method == "POST":
+        content= request.POST.get('content')
+        util.save_entry(title, content)
+        messages.success(request, 'Your entry has been edited successfully !')
+        return redirect('encyclopedia:detail', title)
+
+    context= {'title':title, 'content':content}
+    return render(request, "encyclopedia/edit.html", context)
